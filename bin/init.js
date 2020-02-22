c0baca98122b2a94c2e7214b71b2e476df578faa
hi2048 <291882199@qq.com>
2020-02-22 23:07:02 +0800

    hi2048-cli.

---
+++ bin/init.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const shProcess = require('child_process');
+const package = require('../package');
+const { git } = require('../config/config');
+
+program.version(package.version, '-v, --version')
+  .command('init <name>')
+  .action(name => {
+    console.log(`start cloning ${package.name}...`);
+    shProcess.exec(`git clone ${git} ${name}`, (err, stdout, stderr) => {
+      if(err) {
+        console.log(`exec error ${err}: ${stderr}.`);
+        return;
+      }
+
+      console.log(`${stdout} cloned success.`)
+    });
+  });
+
+program.parse(process.argv);
---
+++ config/config.js
@@ -0,0 +1,7 @@
+const git = 'git@github.com:hi2048/hi2048-scaffold.git';
+
+const config = {
+  git
+};
+
+module.exports = config;
