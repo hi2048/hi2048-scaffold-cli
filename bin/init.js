51d96b9604381096af312e0d6645be646b52287f
hi2048 <291882199@qq.com>
2020-02-23 11:38:19 +0800

    modify pars and del remote origin.

--- bin/init.js
+++ bin/init.js
@@ -6,10 +6,10 @@ const package = require('../package');
 const { git } = require('../config/config');
 
 program.version(package.version, '-v, --version')
-  .command('init <name>')
-  .action(name => {
+  .command('init [name]')
+  .action((name = './') => {
     console.log(`start cloning ${package.name}...`);
-    shProcess.exec(`git clone ${git} ${name}`, (err, stdout, stderr) => {
+    shProcess.exec(`git clone ${git} ${name} && cd ${name} && git remote rm origin`, (err, stdout, stderr) => {
       if(err) {
         console.log(`exec error ${err}: ${stderr}.`);
         return;
